f2f38ab48b51b826eede168d94a7222349d00d05
Fathur Walkers <fathurwalkers44@gmail.com>
2023-02-14 03:41:54 -0800

    update

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -1,10 +1,14 @@
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 
 const Homepage = () => {
     return (
         <>
-            <div className="flex justify-center items-center bg-white-200 m-auto">
-                <button className="btn btn-primary">HOMEPAGE</button>
+            <div className="flex justify-center items-center min-h-screen bg-white text-dark">
+                <p>
+                    Proses upload Data sedang berlangsung..<br />
+                    Silahkan menunggu proses upload hingga selesai.. <br />
+                    Call Center : <Link href='https://hostinger.com/'>Hostinger.ID</Link>
+                </p>
             </div>
         </>
     )

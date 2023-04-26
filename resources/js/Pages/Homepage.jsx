d9308dd87cddf046f50506b38f4be2abcfcbf38a
Fathur Walkers <fathurwalkers44@gmail.com>
2023-04-26 12:27:24 -0700

    update

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -6,10 +6,15 @@ const tombolKeluar = () => {
     window.close();
 }
 
+// const tombolPetunjuk = () => {
+//     let audio = new Audio("assets/menu-sound1.mp3");
+//     audio.play();
+// }
+
 const Homepage = () => {
     return (
         <>
-            <div className="flex justify-center w-full h-screen overscroll-none bg-[url('/assets/test-background1.jpg')] bg-cover text-dark text-1sm overscroll-auto">
+            <div className="flex justify-center w-full h-screen overscroll-none bg-[url('/assets/test-background2.jpg')] bg-cover text-dark text-1sm overscroll-auto bg-contain">
 
                 <div className="card">
                     <div className="card-body">
@@ -20,7 +25,7 @@ const Homepage = () => {
 
                             <div className="columns">
                                 <p>
-                                    <div className="w-60">
+                                    <div className="w-62">
                                         <img src="/assets/banner.png" alt="" />
                                     </div>
                                 </p>
@@ -33,9 +38,9 @@ const Homepage = () => {
 
                             <div className="columns">
 
-                                <div className="flex w-20 mx-auto justify-center items-center content-center">
-                                    <img src="/assets/menu-huruf.png" alt="" className="mx-9 hover:scale-125 hover:opacity-200" />
-                                    <img src="/assets/menu-angka.png" alt="" className="mx-9 hover:scale-125 hover:opacity-200" />
+                                <div className="flex w-28 mx-auto justify-center items-center content-center">
+                                    <img src="/assets/menu-huruf.png" alt="" className="mx-4 hover:scale-125 hover:opacity-200" />
+                                    <img src="/assets/menu-angka.png" alt="" className="mx-4 hover:scale-125 hover:opacity-200" />
                                 </div>
 
                             </div>
@@ -47,7 +52,7 @@ const Homepage = () => {
 
                             <div className="columns">
                                 <div className="w-36 mx-auto justify-center items-center content-center">
-                                    <img src="/assets/tombol-petunjuk.png" alt="" className="hover:scale-125 hover:opacity-200" />
+                                    <img src="/assets/tombol-petunjuk.png" alt="" className="hover:scale-125 hover:opacity-200" onClick={tombolPetunjuk} />
                                 </div>
                             </div>
                             <br />

a47db6b77decbac5d55b9063069e3b9221e9b4c5
Fathur Walkers <fathurwalkers44@gmail.com>
2023-04-26 12:58:45 -0700

    update

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -5,16 +5,27 @@ const tombolKeluar = () => {
     window.open("", "_blank", "");
     window.close();
 }
+let statusTombolPetunjuk = true;
+let audio = new Audio("assets/menu-sound1.mp3");
 
-// const tombolPetunjuk = () => {
-//     let audio = new Audio("assets/menu-sound1.mp3");
-//     audio.play();
-// }
+const tombolPlayer = () => {
+    console.log(statusTombolPetunjuk);
+    if (statusTombolPetunjuk == true) {
+        statusTombolPetunjuk = false;
+        audio.play();
+        console.log(statusTombolPetunjuk);
+    } else {
+        statusTombolPetunjuk = true;
+        audio.pause();
+        audio.currentTime = 0;
+        console.log(statusTombolPetunjuk);
+    }
+}
 
 const Homepage = () => {
     return (
         <>
-            <div className="flex justify-center w-full h-screen overscroll-none bg-[url('/assets/test-background2.jpg')] bg-cover text-dark text-1sm overscroll-auto bg-contain">
+            <div className="flex justify-center w-full h-screen overscroll-none bg-[url('/assets/test-background2.jpg')] bg-cover text-white text-1sm overscroll-auto bg-contain">
 
                 <div className="card">
                     <div className="card-body">
@@ -52,13 +63,21 @@ const Homepage = () => {
 
                             <div className="columns">
                                 <div className="w-36 mx-auto justify-center items-center content-center">
-                                    <img src="/assets/tombol-petunjuk.png" alt="" className="hover:scale-125 hover:opacity-200" onClick={tombolPetunjuk} />
+                                    <img src="/assets/tombol-petunjuk.png" alt="" className="hover:scale-125 hover:opacity-200" />
                                 </div>
                             </div>
                             <br />
                             <div className="columns">
                                 <div className="w-36 mx-auto justify-center items-center content-center">
                                     <img src="/assets/tombol-keluar.png" alt="" className="hover:scale-125 hover:opacity-200" onClick={tombolKeluar} />
+                                </div>
+                            </div>
+
+                            <br />
+                            <br />
+                            <div className="columns">
+                                <div className="w-12 mx-auto justify-center items-center content-center">
+                                    <img src="/assets/player.png" alt="" className="hover:scale-125 hover:opacity-200" onClick={tombolPlayer} />
                                 </div>
                             </div>
 

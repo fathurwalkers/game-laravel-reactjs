767851ecefc07a9e2aaff5b37bdf61c137596e9e
Fathur Walkers <fathurwalkers44@gmail.com>
2023-05-03 14:28:42 -0700

    update

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
